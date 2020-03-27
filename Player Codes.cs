debd2a2dc241024247b3a7724e20ddac44e20dce
Mark Miller <47294360+UnderGuardian@users.noreply.github.com>
2020-03-27 11:14:20 -0600

    Added a common namespace

--- Player Codes.cs
+++ Player Codes.cs
@@ -1,42 +1,45 @@
 using System;
 using System.IO;
 using System.Linq;
-
-class Functions {
-  public string ToHex(int value)
+namespace Crimson
+{
+  class Codes 
   {
-    return String.Format("{0:X}", value);
-  }
-
-  public void Display (int option) {
-    switch(option)
+    public string ToHex(int value)
     {
-      case 1: New(); break;
-      case 2: Load(); break;
-      case 3: Console.WriteLine("No fuck u"); break;
+      return String.Format("{0:X}", value);
     }
-  }
-  public void New()
-  {
-    Random rand = new Random();
-    using (StreamWriter sw = File.AppendText("Users.txt"))
-    {
-      int num = rand.Next(0,80000000);
-      string account = ToHex(num);
-      Console.WriteLine("What's ur name?");
-      string name = Console.ReadLine();
-      sw.WriteLine($"{name}: {account}");
-    }
-  }
 
-  public void Load()
-  {
-    using (StreamReader sr = new StreamReader("Users.txt"))
-    {
-      string lines = "";
-      while((lines = sr.ReadLine()) != null)
+    public void Display (int option) {
+      switch(option)
       {
-        Console.WriteLine(lines);
+        case 1: New(); break;
+        case 2: Load(); break;
+        case 3: Console.WriteLine("No fuck u"); break;
+      }
+    }
+    public void New()
+    {
+      Random rand = new Random();
+      using (StreamWriter sw = File.AppendText("Users.txt"))
+      {
+        int num = rand.Next(0,80000000);
+        string account = ToHex(num);
+        Console.WriteLine("What's ur name?");
+        string name = Console.ReadLine();
+        sw.WriteLine($"{name}: {account}");
+      }
+    }
+
+    public void Load()
+    {
+      using (StreamReader sr = new StreamReader("Users.txt"))
+      {
+        string lines = "";
+        while((lines = sr.ReadLine()) != null)
+        {
+          Console.WriteLine(lines);
+        }
       }
     }
   }
